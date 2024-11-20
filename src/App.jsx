e991b541e52c83b8070a8b4d98d0859e66d3e688
mdoyle91 <mike.w.doyle91@gmail.com>
2024-11-20 11:59:17 -0300

    Initial Setup of Signin, Signup, Dashboard, Navbar, Landing, and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm";
+import * as authService from "../src/services/authService";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState(authService.getUser());
+
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <NavBar user={user} handleSignout={handleSignout} />
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
 
-export default App
+        <Route path="/singup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/services/authService.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+
+const signup = async (formData) => {
+  try {
+    const res = await axios.post(
+      `${VITE_EXPRESS_BACKEND_URL}/users/signup`,
+      formData
+    );
+
+    if (res.err) {
+      throw new Error(res.err);
+    }
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+
+      const user = JSON.parse(atob(res.data.token.split(".")[1]));
+      return user;
+    }
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+};
+
+const signin = async (user) => {
+  try {
+    const res = await axios.post(`${BACKEND_URL}/users/signin`, user);
+
+    if (res.data.error) {
+      throw new Error(res.data.error);
+    }
+
+    if (res.data.token) {
+      localStorage.setItem("token", res.data.token);
+
+      const user = JSON.parse(atob(res.data.token.split(".")[1]));
+      return user;
+    }
+    return res.data;
+  } catch (error) {
+    console.log(err);
+    throw error;
+  }
+};
+
+const getUser = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const user = JSON.parse(atob(token.split(".")[1]));
+  return user;
+};
+
+const signout = () => {
+  localStorage.remoeItem("token");
+};
+
+export { signup, signin, getUser, signout };
